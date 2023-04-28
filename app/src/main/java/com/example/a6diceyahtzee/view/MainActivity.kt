d4d1fed259ffe11a5556e50d7bb8a4263ecb6d8d
Drakepal <playonplay23@gmail.com>
2023-04-28 14:46:07 -0500

    done with part 12

--- app/src/main/java/com/example/a6diceyahtzee/view/MainActivity.kt
+++ app/src/main/java/com/example/a6diceyahtzee/view/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.a6diceyahtzee.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.LinearLayout
+import android.view.ViewGroup
+import android.widget.EditText
 import androidx.activity.viewModels
 import com.example.a6diceyahtzee.R
 import com.example.a6diceyahtzee.databinding.ActivityMainBinding
 import com.example.a6diceyahtzee.databinding.ContentMainBinding
 import com.example.a6diceyahtzee.model.Dice
 import com.example.a6diceyahtzee.model.Player
+import com.example.a6diceyahtzee.model.Set
 import com.example.a6diceyahtzee.viewmodel.DiceRecyclerViewAdapter
 import com.example.a6diceyahtzee.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -48,11 +50,136 @@ class MainActivity : AppCompatActivity(), NewGameDialogue.NewGameDialogEvents, D
         Log.d(TAG, "onCreateDialogResult: ends")
     }
 
-    private fun clearForm(linearLayoutScores: LinearLayout) {
-        TODO("Not yet implemented")
+    private fun clearForm(group: ViewGroup) {
+        var i = 0
+        val count = group.childCount
+        while(i < count) {
+            val view = group.getChildAt(i)
+            if(view is EditText) {
+                view.setText("")
+            }
+            if (view is ViewGroup && view.childCount > 0) clearForm(view)
+            ++i
+        }
     }
 
     override fun onSaveClick(diceItem: Dice) {
-        TODO("Not yet implemented")
+        Log.d(TAG, "onSaveClick: called with $diceItem")
+        mainViewModel.savePlayerDice(diceItem)
+        Log.d(TAG, "onSaveClick: ends with $diceItem")
+    }
+
+    private fun changeStateFirstPlayerBoxScores(enable: Boolean) {
+
+        contentMainBinding.apply {
+            enable.let {
+                etDiceOneFirstPlayer.isEnabled = it
+                etDiceTwoFirstPlayer.isEnabled = it
+                etDiceThreeFirstPlayer.isEnabled = it
+                etDiceFourFirstPlayer.isEnabled = it
+                etDiceFiveFirstPlayer.isEnabled = it
+                etDiceSixFirstPlayer.isEnabled = it
+                etThreeOfKindFirstPlayer.isEnabled = it
+                etFourOfKindFirstPlayer.isEnabled = it
+                etFullHouseFirstPlayer.isEnabled = it
+                etSmallStraightFirstPlayer.isEnabled = it
+                etLargeStraightFirstPlayer.isEnabled = it
+                etYahtzeeFirstPlayer.isEnabled = it
+                etChanceFirstPlayer.isEnabled = it
+            }
+        }
+    }
+
+    private fun changeStateSecondPlayerBoxScores(enable: Boolean) {
+
+        contentMainBinding.apply {
+            enable.let {
+                etDiceOneSecondPlayer.isEnabled = it
+                etDiceTwoSecondPlayer.isEnabled = it
+                etDiceThreeSecondPlayer.isEnabled = it
+                etDiceFourSecondPlayer.isEnabled = it
+                etDiceFiveSecondPlayer.isEnabled = it
+                etDiceSixSecondPlayer.isEnabled = it
+                etThreeOfKindSecondPlayer.isEnabled = it
+                etFourOfKindSecondPlayer.isEnabled = it
+                etFullHouseSecondPlayer.isEnabled = it
+                etSmallStraightSecondPlayer.isEnabled = it
+                etLargeStraightSecondPlayer.isEnabled = it
+                etYahtzeeSecondPlayer.isEnabled = it
+                etChanceSecondPlayer.isEnabled = it
+            }
+        }
+    }
+
+    private fun setPlayerTurnOn(player: Player) {
+        diceRecyclerViewAdapter.loadNewDices(player.diceToRoll)
+        when {
+            player.rollCount <= 0 -> {
+                contentMainBinding.btnRoll.isEnabled = false
+            } else -> {
+                contentMainBinding.btnRoll.isEnabled = true
+            }
+        }
+
+        contentMainBinding.btnRoll.text = getString(
+            R.string.btn_roll_in_game,
+            player.name,
+            player.rollCount.toString()
+        )
+    }
+
+    private fun setFirstPlayer(firstPlayer: Player) {
+
+        contentMainBinding.apply {
+            firstPlayer.name.let {
+                tvFirstPlayerLeft.text = it
+                tvFirstPlayerRight.text = it
+                tvFirstPlayerName.text = it
+
+                firstPlayer.setScores.forEach {
+                    when (it.key) {
+                        Set.ACES -> {
+                            etDiceOneFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.TWOS -> {
+                            etDiceTwoFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.THREES -> {
+                            etDiceThreeFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.FOURS -> {
+                            etDiceFourFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.FIVES -> {
+                            etDiceFiveFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.SIXES -> {
+                            etDiceSixFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.THREE_OF_A_KIND -> {
+                            etThreeOfKindFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.FOUR_OF_A_KIND -> {
+                            etFourOfKindFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.FULL_HOUSE -> {
+                            etFullHouseFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.SMALL_STRAIGHT -> {
+                            etSmallStraightFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.LARGE_STRAIGHT -> {
+                            etLargeStraightFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.YAHTZEE -> {
+                            etYahtzeeFirstPlayer.setText(it.value.toString())
+                        }
+                        Set.CHANCE -> {
+                            etChanceFirstPlayer.setText(it.value.toString())
+                        }
+                    }
+                }
+            }
+        }
     }
 }
